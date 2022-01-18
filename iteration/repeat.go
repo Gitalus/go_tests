50e0f78a0e9b35591245fc4df46fa663bb967d37
Italo Guajardo <italog.penaloza@gmail.com>
2022-01-17 21:49:18 -0300

    Improve repeat func using strings library

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,12 +1,12 @@
 package iteration
 
+import "strings"
+
 const repeatCounter = 5
 
 // Repeat 5 times the given character
 func Repeat(character string) string {
-	var repeated string
-	for i := 0; i < repeatCounter; i++ {
-		repeated += character
-	}
+	var repeated string = strings.Repeat(character, repeatCounter)
+
 	return repeated
 }
