75018c783fe7f9b7efdb0a287de23ff732d10587
Italo Guajardo <italog.penaloza@gmail.com>
2022-01-17 21:43:12 -0300

    Create Benchmark for Repeat

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,11 @@
 package iteration
 
+const repeatCounter = 5
+
 // Repeat 5 times the given character
 func Repeat(character string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < repeatCounter; i++ {
 		repeated += character
 	}
 	return repeated
