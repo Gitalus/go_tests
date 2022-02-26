bb8eaa4ffc48d4d36cf7ba664d813ddbe2663655
Italo Guajardo <italog.penaloza@gmail.com>
2022-02-25 21:03:33 -0300

    Create Area function with its test

--- structs_methods_interface/shapes.go
+++ structs_methods_interface/shapes.go
@@ -3,3 +3,8 @@ package structs_methods_interface
 func Perimeter(width, height float64) float64 {
 	return 2 * (width + height)
 }
+
+// area can be in lowecase because is only being used on the same package
+func Area(width, height float64) float64 {
+	return width * height
+}
