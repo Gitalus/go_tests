9cdf75e9ec846e477c17d12cfd5578970b0885ee
Italo Guajardo <italog.penaloza@gmail.com>
2022-02-26 14:37:08 -0300

    Format bitcoin to print it at different format

--- pointers_errors/wallet.go
+++ pointers_errors/wallet.go
@@ -1,6 +1,19 @@
 package pointers_errors
 
+import "fmt"
+
+type Stringer interface {
+	String() string
+}
+
+// Esto nos reconoce el tipo Bitcoin como distinto de int
 type Bitcoin int
+
+// String() se llama cuando se imprime en métodos de fmt
+func (b Bitcoin) String() string {
+	// Formatea la string y la devuelve
+	return fmt.Sprintf("%d BTC", b)
+}
 
 type Wallet struct {
 	balance Bitcoin
