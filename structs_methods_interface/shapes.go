d4ccdd8190290cdb00fa419473c95ef6ecb73c82
Italo Guajardo <italog.penaloza@gmail.com>
2022-02-25 21:10:29 -0300

    Refactro shapes function to use a struct

--- structs_methods_interface/shapes.go
+++ structs_methods_interface/shapes.go
@@ -1,10 +1,15 @@
 package structs_methods_interface
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.width + rectangle.height)
 }
 
 // area can be in lowecase because is only being used on the same package
-func Area(width, height float64) float64 {
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.width * rectangle.height
 }
