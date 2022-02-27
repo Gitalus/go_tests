520593eea0a3813ada90f5b72f36ea1f2bc826aa
Italo Guajardo <italog.penaloza@gmail.com>
2022-02-26 22:09:02 -0300

    Include second return from map to check if item exists

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,16 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	// maps lookup pueden retornar dos valores, el segundo un bool si encuentra la llave o no
+	definition, ok := d[word]
+
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return definition, nil
 }
