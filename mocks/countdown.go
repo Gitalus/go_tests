7ddba148441d617091469bbaa090bb9beb29c2ec
Italo Guajardo <italog.penaloza@gmail.com>
2022-02-28 21:20:58 -0300

    Refactor code to include only new struc that contains Sleep and Write on test

--- mocks/countdown.go
+++ mocks/countdown.go
@@ -4,12 +4,52 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
-func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+	write          = "write"
+	sleep          = "sleep"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	// Este approach es muy recomendado y bueno, permite muchas cosas y funciona e2e
+	// El test demorará 4 segundos, lo que empeora la productividad
+	for i := countdownStart; i > 0; i-- {
+		// ¿La deferrencia del puntero se hace automática cuando se ejecuta un método?
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprint(out, finalWord)
 }
--- mocks/countdown_test.go
+++ mocks/countdown_test.go
@@ -2,24 +2,46 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestCountdown(t *testing.T) {
-	/*
-		Necesitaremos testear un iterative.
-		De esta forma evitamos hackear y tendremos un código más limpio.
-		Es importante dividir los problemas en pequeños para tener un software
-		funcional
-	*/
-	buffer := &bytes.Buffer{}
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
 
-	Countdown(buffer)
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
 
-	got := buffer.String()
-	want := "3"
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		/*
+			Necesitaremos testear un iterative.
+			De esta forma evitamos hackear y tendremos un código más limpio.
+			Es importante dividir los problemas en pequeños para tener un software
+			funcional
+		*/
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &SpyCountdownOperations{})
+
+		got := buffer.String()
+		want := "3\n2\n1\nGo!"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
