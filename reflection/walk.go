c5d0a9006452e12781d54f1dfab12987afe26ba4
Italo Guajardo <italog.penaloza@gmail.com>
2022-04-08 18:00:20 -0400

    Finish first iteration of walk function

--- reflection/walk.go
+++ reflection/walk.go
@@ -1,1 +1,16 @@
 package walk
+
+import "reflect"
+
+func walk(x interface{}, fn func(input string)) {
+	// reflect.ValueOf get the value of a given variable (x)
+	val := reflect.ValueOf(x)
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
+	}
+}
