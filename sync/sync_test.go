52f6777ba0a1507533c4b7d31d1485dd504e5abe
Italo Guajardo <italog.penaloza@gmail.com>
2022-04-09 15:58:30 -0400

    Add suport for correct implement of mutex

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,19 +1,46 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
 
 		assertCounter(t, counter, 3)
 	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+
+		wg.Add(wantedCount) // Time to wait for 'Dones'
+		for i := 0; i < wantedCount; i++ {
+			// This will fail sometimes because every iteration wants to change the value,
+			// sometimes at the same time
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+	})
 }
 
-func assertCounter(t testing.TB, got Counter, want int) {
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func assertCounter(t testing.TB, got *Counter, want int) {
 	t.Helper()
 	if got.Value() != want {
 		t.Errorf("got %d, want %d", got.Value(), want)
