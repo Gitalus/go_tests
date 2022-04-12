422750ee818fae5adfe123f7b8c959a6ef77c33b
Italo Guajardo <italog.penaloza@gmail.com>
2022-04-11 22:37:55 -0400

    Refactor ConverToRoman to get a first elegant approach

--- property_based/toroman.go
+++ property_based/toroman.go
@@ -3,10 +3,21 @@ package property_based
 import "strings"
 
 func ConvertToRoman(arabic int) string {
+	// A Builder is used to efficiently build a string using Write methods. It minimizes memory copying.
 	var result strings.Builder
 
-	for i := 0; i < arabic; i++ {
-		result.WriteString("I")
+	for arabic > 0 {
+		switch {
+		case arabic > 4:
+			result.WriteString("V")
+			arabic -= 5
+		case arabic > 3:
+			result.WriteString("IV")
+			arabic -= 4
+		default:
+			result.WriteString("I")
+			arabic--
+		}
 	}
 
 	return result.String()
