80c4aea9bad88a7a9bfaa45869b83b326821ab1e
Italo Guajardo <italog.penaloza@gmail.com>
2022-04-17 18:34:31 -0400

    Create test for SecondsInRadian and add test cases

--- clockface/clockface_test.go
+++ clockface/clockface_test.go
@@ -1,6 +1,7 @@
 package clockface
 
 import (
+	"math"
 	"testing"
 	"time"
 )
@@ -24,5 +25,26 @@ func TestSecondHandAt30Seconds(t *testing.T) {
 
 	if got != want {
 		t.Errorf("got %v, wanted %v", got, want)
+	}
+}
+
+func TestSecondsInRadian(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(0, 0, 30), math.Pi},
+		{simpleTime(0, 0, 30), 0},
+		{simpleTime(0, 0, 30), (math.Pi / 2) * 3},
+		{simpleTime(0, 0, 30), (math.Pi / 30) * 7},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := secondsInRadians(c.time)
+			if got != c.angle {
+				t.Fatalf("Wanted %v radians, but got %v", want, got)
+			}
+		})
 	}
 }
