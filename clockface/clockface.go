a3b06c42dccdb02a4725901d1ca6b6b53fb7a324
Italo Guajardo <italog.penaloza@gmail.com>
2022-04-19 22:26:45 -0400

    Make some progress on testing xml parse

--- clockface/clockface.go
+++ clockface/clockface.go
@@ -1,6 +1,8 @@
 package clockface
 
 import (
+	"fmt"
+	"io"
 	"math"
 	"time"
 )
@@ -17,15 +19,22 @@ const (
 	clockCenterY     = 150
 )
 
+//SVGWriter writes an SVG representation of an analogue clock, showing the time t, to the writer w
+func SVGWriter(w io.Writer, t time.Time) {
+	io.WriteString(w, svgStart)
+	io.WriteString(w, bezel)
+	SecondHand(w, t)
+	io.WriteString(w, svgEnd)
+}
+
 // SecondHand is the unit vector of the second hand of a analogue clock at the time `t`
 // represented as a Point.
-func SecondHand(t time.Time) Point {
+func SecondHand(w io.Writer, t time.Time) {
 	p := secondHandPoint(t)
-	p = Point{p.X * secondHandLength, p.Y * secondHandLength}
-	p = Point{p.X, -p.Y}
-	p = Point{p.X + clockCenterX, p.Y + clockCenterY}
-
-	return p
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale
+	p = Point{p.X, -p.Y}                                      // flip
+	p = Point{p.X + clockCenterX, p.Y + clockCenterY}         // translate
+	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%f" y2="%f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
 }
 
 func secondsInRadians(t time.Time) float64 {
@@ -51,3 +60,15 @@ func secondHandPoint(t time.Time) Point {
 
 	return Point{x, y}
 }
+
+const svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg xmlns="http://www.w3.org/2000/svg"
+     width="100%"
+     height="100%"
+     viewBox="0 0 300 300"
+     version="2.0">`
+
+const bezel = `<circle cx="150" cy="150" r="100" style="fill:#fff;stroke:#000;stroke-width:5px;"/>`
+
+const svgEnd = `</svg>`
